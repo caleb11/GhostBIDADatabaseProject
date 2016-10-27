93128e0556f8e3f1297174ceacaf4ad683cbd0c3
caleb11 <cthompson11@babson.edu>
2016-10-27 00:07:28 -0400

    Add files via upload

---
+++ Ghost_dbcreation.sql
@@ -0,0 +1,44 @@
+﻿CREATE DATABASE Amazucc;
+GO
+
+USE Amazucc;
+GO
+
+/*Create the customer and order tables*/
+CREATE TABLE Sales(
+  SalesID bigint NOT NULL PRIMARY KEY,
+  OrderDate varchar(50) NOT NULL,
+  ShipDate varchar(50) NOT NULL,
+  RegionID bigint NOT NULL,
+  ProductID bigint NOT NULL,
+  EmployeeID bigint NOT NULL,
+  SalesTotal varchar(50) NOT NULL,
+  CustomerID varchar(50) NOT NULL,
+  OrderQuantity varchar(50) NOT NULL);
+
+  CREATE TABLE Product(
+  ProductID bigint NOT NULL PRIMARY KEY,
+  CustomerID varchar(50) NOT NULL,
+  EnglishProductName varchar(50) NOT NULL,
+  StatusOfProduct varchar(50) NOT NULL, 
+  RegionID bigint NOT NULL);
+
+  CREATE TABLE Region(
+  RegionID bigint NOT NULL PRIMARY KEY,
+  Country varchar(50) NOT NULL);
+
+  CREATE TABLE Employee(
+  EmployeeID bigint NOT NULL PRIMARY KEY,
+  EmployeeName varchar(50) NOT NULL,
+  RegionID varchar(50) NOT NULL,
+  Gender varchar(50) NOT NULL);
+
+  
+
+
+
+
+
+
+
+
---
+++ Ghost_dbqueries.sql
@@ -0,0 +1,50 @@
+﻿CREATE DATABASE Amazucc;
+GO
+
+USE Amazucc;
+GO
+
+/*Multiple SELECT * queries to display all the records from each table. */
+ Select *
+ From Region
+
+ /*Multiple SELECT * queries to display all the records from each table. */
+ Select *
+ From Product
+
+ /*Multiple SELECT * queries to display all the records from each table. */
+ Select *
+ From Sales
+
+ /*Multiple SELECT * queries to display all the records from each table. */
+ Select *
+ From Employee
+
+/*A query that uses an aggregate function*/
+Select Sum(SalesTotal)
+From Sales
+
+/*A query that selects records from two (or multiple) tables using INNER JOIN.*/
+Select CR.RegionID, ST. Country
+From dbo.Sales as CR
+inner join dbo.Region as ST
+ON CR. RegionID = ST. RegionID;
+
+/*A query that selects records from two (or multiple) tables using LEFT OUTER JOIN (or RIGHT OUTER JOIN or FULL OUTER JOIN).*/
+Select C.RegionID, E. EnglishProductName, *
+From dbo.Sales as C
+full outer join dbo.Product as E
+ON C. RegionID = E. RegionID;
+/* Full Outer join combines the tables to form one set of data that puts all of the correspsonding attributes in one place to look at easily.
+
+/*A query that uses a subquery*/
+/*Devan Sood, Caleb Thompson, Ian Garland*/
+  
+
+
+
+
+
+
+
+
